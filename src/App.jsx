64abc8db29c511ec4063eb6cbdf811fa290a8e30
Leedoseo <okok0840@naver.com>
2026-02-27 22:52:47 +0900

    반응형 디자인 및 UI 개선 (햄버거, 검색, 트랜지션)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/common/Layout';
-import HomePage from './pages/HomePage';
-import ProductListPage from './pages/ProductListPage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import CartPage from './pages/CartPage';
-import WishlistPage from './pages/WishlistPage';
-import OrderPage from './pages/OrderPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/common/Layout";
+import PageTransition from "./components/common/PageTransition";
+import HomePage from "./pages/HomePage";
+import ProductListPage from "./pages/ProductListPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import CartPage from "./pages/CartPage";
+import WishlistPage from "./pages/WishlistPage";
+import OrderPage from "./pages/OrderPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<ProductListPage />} />
-          <Route path="/products/:id" element={<ProductDetailPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/wishlist" element={<WishlistPage />} />
-          <Route path="/order" element={<OrderPage />} />
-        </Routes>
+        <PageTransition>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products" element={<ProductListPage />} />
+            <Route path="/products/:id" element={<ProductDetailPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/wishlist" element={<WishlistPage />} />
+            <Route path="/order" element={<OrderPage />} />
+          </Routes>
+        </PageTransition>
       </Layout>
     </BrowserRouter>
   );
